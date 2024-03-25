d76beb529710681a9ed4b05d453204e16acac115
Dalton Wright <94549091+DWright91@users.noreply.github.com>
2024-03-25 16:50:09 -0500

    Add files via upload.

---
+++ wordcountMariaDBTest.sql
@@ -0,0 +1,68 @@
+--------------------------------------------------------------------------------
+-- wordcountMariaDBTest.sql                                                   --
+-- SE 413 Project1                                                            --
+-- Created by Dalton, Sivty, and Winston                                      --
+--------------------------------------------------------------------------------
+
+-- Open MariaDB and enter your password
+-- Run command: source .\..\..\wikipedia-dataset\wordcountMariaDBTest.sql
+
+-- Set timeout to 600 seconds (10 minutes)
+SET GLOBAL innodb_lock_wait_timeout = 600;
+SET innodb_lock_wait_timeout = 600;
+
+CREATE DATABASE wikipedia;
+
+USE wikipedia;
+
+-- Start time.
+SELECT
+    NOW();
+
+-- Load words from input.txt into a table called "words".
+CREATE TABLE words (word varchar(20));
+
+-- LOAD DATA LOCAL INFILE './../../wikipedia-dataset/test.xml'
+-- LOAD DATA LOCAL INFILE './../../wikipedia-dataset/enwiki-20230101-pages-meta-history1.xml-p1p857'
+LOAD DATA LOCAL INFILE './../../wikipedia-dataset/wikipedia-subset.xml'
+INTO TABLE words
+LINES TERMINATED BY ' ';
+
+-- SQL Query to create a table called "top_words"
+-- for the three most frequently used words.
+CREATE TABLE top_words AS (
+    SELECT
+        word,
+        COUNT(*) as frequency
+    FROM
+        words
+    WHERE
+        word != ' '
+    GROUP BY
+        word
+    ORDER BY
+        frequency DESC
+    LIMIT
+        3
+);
+
+-- Create a view of the "top_words" table.
+-- CREATE VIEW top_words_view AS
+-- SELECT word, frequency
+-- FROM top_words;
+-- mariadb-show top_words;
+SELECT
+    *
+FROM
+    top_words;
+
+-- Stop time.
+SELECT
+    NOW();
+
+-- DROP VIEW top_words_view;
+DROP TABLE words;
+
+DROP TABLE top_words;
+
+DROP DATABASE wikipedia;
---
+++ wordcountSparkTest.py
@@ -0,0 +1,63 @@
+################################################################################
+## wordcountSparkTest.py                                                      ##
+## SE 413 Project1                                                            ##
+## Created by Dalton, Sivty, and Winston                                      ##
+################################################################################
+
+# Open Command Prompt
+# Navigate to directory D:/wikipedia-dataset
+# Run command: spark-submit wordcountSparkTest.py
+
+from pyspark import SparkContext, SparkConf
+from collections import Counter
+import time
+
+# Start the timer
+start = time.time()
+
+# set up Spark context
+conf = (SparkConf().setMaster("local")
+                    .setAppName("word-count")
+                    .set("spark.executor.memory", "10g")) # can set to 1tb.
+sc = SparkContext(conf = conf)
+
+# read in the file
+# filename = "D:\wikipedia-dataset\enwiki-20230101-pages-meta-history1.xml-p1p857"
+# filename = "input.txt"
+# filename = "enwiki-20230101-pages-meta-history1.xml-p1p857"
+filename = "wikipedia-subset.xml"
+text_file = sc.textFile(filename)
+
+# split the text into words
+words = text_file.flatMap(lambda line: line.lower().split())
+
+# count the occurrences of each word
+word_counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+# sort the words by count
+sorted_words = word_counts.takeOrdered(word_counts.count(), lambda x: -x[1])
+
+# print("List of words in dataset:")
+
+# print the words and their counts
+# for word, count in sorted_words:
+#     print(f"{word}: {count}")
+
+# print the top three words and their counts
+for x in range(3):
+    print(f"{sorted_words[x]}")
+
+# get the three most common words
+# top_words = word_counts.takeOrdered(3, lambda x: -x[1])
+
+# assign the top words as keywords
+# keywords = [word[0] for word in top_words]
+
+# print(f"The top three keywords are: {keywords}")
+sc.stop()
+
+# Stop the timer
+end = time.time()
+
+# Print the time taken
+print(f"Total time taken: {end - start}")
